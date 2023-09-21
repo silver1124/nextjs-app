1092e1b550d3fe6b6863f560ddd1c1c33e9b8d93
silver1124 <yashtiwari1124>
2023-09-21 23:46:01 +0530

    Final touch to About US page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,4 +1,21 @@
+import Link from "next/link";
+
 const AboutUs = () => {
-    return <h1>About Us Page...</h1>;
-  };
-  export default AboutUs;
+  return (
+    <>
+      <h1>About Us Page...</h1>;
+      <ul>
+        <li>
+          <Link href="/aboutus/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/3">Suresh</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+export default AboutUs;
