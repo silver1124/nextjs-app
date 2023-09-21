4f56c3d74151c20b8ece92104dc586dacee59d1f
silver1124 <yashtiwari1124>
2023-09-21 16:34:37 +0530

    Making Nested Routes

---
+++ pages/aboutus/developer/index.js
@@ -0,0 +1,4 @@
+function Developer() {
+    return <h1>Yash Tiwari</h1>;
+  }
+  export default Developer;
---
+++ pages/news/news.js
@@ -0,0 +1,4 @@
+function Detail() {
+    return <h1>News Details are as follows</h1>;
+  }
+  export default Detail;
